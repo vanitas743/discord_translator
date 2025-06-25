b818dc03a375702a84faf8f70505030b73d27dda
ばにたす <yudon.2002@gmail.com>
2025-06-26 00:14:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 import os
-import threading
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Bot is running!"
-
-def run_server():
-    app.run(host="0.0.0.0", port=8000)
-
-threading.Thread(target=run_server).start()
 
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
